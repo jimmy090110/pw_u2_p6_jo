528a84020f9223678c869efec0c27a7a66e8f8b0
Sebastián Carrera <sebitas5225@gmail.com>
2024-05-29 19:33:22 -0500

    Taller13

--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
-console.log(Vue)
+console.log(Vue);
+
+const estudiantes = [{ nombre: "Jimmy", apellido: "Ortega" },
+{ nombre: "Juan", apellido: "Teran" },
+{ nombre: "Lucas", apellido: "Cayambe" },
+{ nombre: "Andrea", apellido: "Paez" },
+{ nombre: "Tania", apellido: "Castillo" },
+{ nombre: "Pedro", apellido: "Martinez" }
+];
+
+const app = Vue.createApp({
+  /* template: `
+     <div>
+       <h1>Hola Mundo desde Vue.js</h1>
+       <p>{{ 1 + 1 }}</p>
+       <p>{{[1,2,3,4]}}</p>
+       <p>{{[true?'True':'False']}}</p>
+     </div>
+   `
+   */
+
+  //Propiedades reactivas
+  data() {
+    return {
+      mensaje: "Hola Mundo Propiedad Reactiva",
+      valor: 7,
+      lista:estudiantes,
+      
+    }
+  },
+  //metodos
+  methods: {
+    cambiarTexto() {  // toda propiedad reactiva debe utilizar la palabra this
+      this.mensaje = "Texto Cambiado";
+
+    },
+    sumar() {
+      this.valor = this.valor + 100;
+    },
+    agregar(){
+      const nuevo={
+        nombre:"Jimmy",
+        apellido:"Ortega"
+      }
+      this.lista.unshift(nuevo);
+
+    }
+
+
+  }
+
+});
+
+app.mount('#myApp');
